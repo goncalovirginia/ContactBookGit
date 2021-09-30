e898f543e2cb8a02447fc87360c7e13ecdc13153
Gonçalo <gonvirginia.blueslime@gmail.com>
2021-10-01 00:39:21 +0100

    Tabs > Spaces

--- src/Tests.java
+++ src/Tests.java
@@ -22,71 +22,71 @@ import org.junit.Test;
  * Veja como o pode fazer na primeira aula prática do semestre!
  */
 public class Tests {
-    /**
-     * Use as linhas que se seguem para especificar os testes que vai realizar.
-     * Neste ficheiro de exemplo, criado para o projecto contactBook.ContactBook, apenas temos
-     * 3 testes para realizar. Para cada ficheiro de input, existe um ficheiro de
-     * output correspondente. Por exemplo, o resultado esperado para o teste
-     * 1_in_base.txt é 1_out_base.txt . Não tem de fazer mais nada no resto da classe.
-     * Basta configurar esta sequência de testes!
-     */
-    @Test public void test1() { test("1_in_base.txt","1_out_base.txt"); }
-    @Test public void test2() { test("2_in_base_GN.txt","2_out_base_GN.txt"); }
-    @Test public void test3() { test("3_in_base_EP.txt","3_out_base_EP.txt"); }
-
-
-    private static final File BASE = new File("tests");
-
-    private PrintStream consoleStream;
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-
-    @Before
-    public void setup() {
-        consoleStream = System.out;
-        System.setOut(new PrintStream(outContent));
-    }
-
-    public void test(String intput, String output) {
-        test(new File(BASE, intput), new File(BASE, output));
-    }
-
-    public void test(File input, File output) {
-        consoleStream.println("Testing!");
-        consoleStream.println("Input: " + input.getAbsolutePath());
-        consoleStream.println("Output: " + output.getAbsolutePath());
-
-        String fullInput = "", fullOutput = "";
-        try {
-            fullInput = new String(Files.readAllBytes(input.toPath()));
-            fullOutput = new String(Files.readAllBytes(output.toPath()));
-            consoleStream.println("INPUT ============");
-            consoleStream.println(new String(fullInput));
-            consoleStream.println("OUTPUT ESPERADO =============");
-            consoleStream.println(new String(fullOutput));
-            consoleStream.println("OUTPUT =============");
-        } catch(Exception e) {
-            e.printStackTrace();
-            fail("Erro a ler o ficheiro");
-        }
-
-        try {
-            Locale.setDefault(Locale.US);
-            System.setIn(new FileInputStream(input));
-            Class<?> mainClass = Class.forName("Main");
-            mainClass.getMethod("main", String[].class).invoke(null, new Object[] { new String[0] });
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail("Erro no programa");
-        } finally {
-            byte[] outPrintBytes = outContent.toByteArray();
-            consoleStream.println(new String(outPrintBytes));
-
-            assertEquals(removeCarriages(fullOutput), removeCarriages(new String(outContent.toByteArray())));
-        }
-    }
-
-    private static String removeCarriages(String s) {
-        return s.replaceAll("\r\n", "\n");
-    }
-
+	/**
+	 * Use as linhas que se seguem para especificar os testes que vai realizar.
+	 * Neste ficheiro de exemplo, criado para o projecto contactBook.ContactBook, apenas temos
+	 * 3 testes para realizar. Para cada ficheiro de input, existe um ficheiro de
+	 * output correspondente. Por exemplo, o resultado esperado para o teste
+	 * 1_in_base.txt é 1_out_base.txt . Não tem de fazer mais nada no resto da classe.
+	 * Basta configurar esta sequência de testes!
+	 */
+	@Test public void test1() { test("1_in_base.txt","1_out_base.txt"); }
+	@Test public void test2() { test("2_in_base_GN.txt","2_out_base_GN.txt"); }
+	@Test public void test3() { test("3_in_base_EP.txt","3_out_base_EP.txt"); }
+	
+	
+	private static final File BASE = new File("tests");
+	
+	private PrintStream consoleStream;
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	
+	@Before
+	public void setup() {
+		consoleStream = System.out;
+		System.setOut(new PrintStream(outContent));
+	}
+	
+	public void test(String intput, String output) {
+		test(new File(BASE, intput), new File(BASE, output));
+	}
+	
+	public void test(File input, File output) {
+		consoleStream.println("Testing!");
+		consoleStream.println("Input: " + input.getAbsolutePath());
+		consoleStream.println("Output: " + output.getAbsolutePath());
+		
+		String fullInput = "", fullOutput = "";
+		try {
+			fullInput = new String(Files.readAllBytes(input.toPath()));
+			fullOutput = new String(Files.readAllBytes(output.toPath()));
+			consoleStream.println("INPUT ============");
+			consoleStream.println(new String(fullInput));
+			consoleStream.println("OUTPUT ESPERADO =============");
+			consoleStream.println(new String(fullOutput));
+			consoleStream.println("OUTPUT =============");
+		} catch(Exception e) {
+			e.printStackTrace();
+			fail("Erro a ler o ficheiro");
+		}
+		
+		try {
+			Locale.setDefault(Locale.US);
+			System.setIn(new FileInputStream(input));
+			Class<?> mainClass = Class.forName("Main");
+			mainClass.getMethod("main", String[].class).invoke(null, new Object[] { new String[0] });
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Erro no programa");
+		} finally {
+			byte[] outPrintBytes = outContent.toByteArray();
+			consoleStream.println(new String(outPrintBytes));
+			
+			assertEquals(removeCarriages(fullOutput), removeCarriages(new String(outContent.toByteArray())));
+		}
+	}
+	
+	private static String removeCarriages(String s) {
+		return s.replaceAll("\r\n", "\n");
+	}
+	
 }
